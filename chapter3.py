97dc7823c0dfa2ff5093e9270908a0aec391b654
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-01-25 10:10:55 +0200

    Update and rename chapter2.py to chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -54,3 +54,36 @@ inconsistent_ages = banking[~age_equ]
 
 # Store consistent and inconsistent data
 print("Number of inconsistent ages: ", inconsistent_ages.shape[0])
+
+---------------------
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values('age')
+msno.matrix(banking_sorted)
+plt.show()
+-----------------
+# Drop missing values of cust_id
+banking_fullid = banking.dropna(subset = ['cust_id'])
+
+# Compute estimated acct_amount
+acct_imp = banking_fullid['inv_amount'] * 5
+
+# Impute missing acct_amount with corresponding acct_imp
+banking_imputed = banking_fullid.fillna({'acct_amount':acct_imp})
+
+# Print number of missing values
+print(banking_imputed.isna().sum())
+
+
+
+
